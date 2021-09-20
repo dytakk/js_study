2a45c08b9536b47f9072956702d302904c2cf0f4
dytakk <telecaster0@naver.com>
2021-09-20 23:29:08 +0900

    form moving page study

--- src/main/webapp/scripts/main.js
+++ src/main/webapp/scripts/main.js
@@ -39,3 +39,25 @@ myButton.onclick = function(){
 	setUserName();
 	
 }
+
+function pageMove(){
+/*form의 name을 page혹은 button의 name을 page로 줘도 된다.
+여러정보를 담아 보낼땐 form태그 + appendChild(수업시간에 배운 방식)
+한가지에 대해 보낼떈 버튼에 name 혹은 id를 부여해도 되는거 같다.
+name은 중복해서 적을수있으니 여러개를 보낼수있음! (button들에게 같은 name을 부여)
+혹은 form에 name이나 id를주면 지정한 form태그안의 값들만 보낼수있겠지!*/
+
+	var move = document.page;	
+	move.submit();
+	
+}
+
+function pageMove2(something){
+
+	alert(something);
+	
+	
+}
+
+
+
